443f03f8b7d7703cae49e8996329e6256cb3585d
Ali Morakabi <alimkb@gmail.com>
2021-03-25 19:27:13 +0400

    commit

---
+++ rss2mail.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  untitled.py
+#  
+#  Copyright 2021 Ali Morakabi <alimkb@gmail.com>
+#  
+# https://news.google.com/news/rss/headlines/section/topic/{topic}
+
+#install lxml : pip3 install lxml
+
+import requests
+from bs4 import BeautifulSoup
+
+
+interval = 360 # in minutes
+
+urls = ['https://news.yahoo.com/rss/',
+        'https://news.google.com/news/rss']
+
+words = ['apple','best','stocks']
+
+items = []
+
+# Rss reader function
+def fetch_rss(addr) :
+	try:
+		r = requests.get(addr)
+		soup = BeautifulSoup(r.content, features='xml')
+		items = soup.findAll('item')
+		return items
+
+	except Exception as e:
+		print('The scraping job failed. See exception: ')
+		print(e)
+
+
+
+print('Starting scraping :')
+for i in urls:
+	print(fetch_rss(i))
+	
+print('--- Finished scraping ---')
+	
